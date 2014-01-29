cad6a42ae91bd85f706d90f19c48586547ee1c2b
Jacky Chen <jingchen@cmu.edu>
2014-01-28 18:39:14 -0800

    version 3

--- matrix_mul/omp/matrix_mul.cpp
+++ matrix_mul/omp/matrix_mul.cpp
@@ -18,6 +18,7 @@
 
 #include <omp.h>
 #include <iostream>
+#include <emmintrin.h>
 #include "matrix_mul.h"
 
 namespace omp
@@ -33,57 +34,105 @@ void matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2,
 			sq_matrix_2_tmp[i*sq_dimension+j] = sq_matrix_2[j*sq_dimension+i];
 		}
 	}
-    
-#pragma omp parallel for if(sq_dimension > 200)
-	for (unsigned int i = 0; i < sq_dimension; i++) 
-    {
-		for(unsigned int j = 0; j < sq_dimension; j++) 
-		{       
-			sq_matrix_result[i*sq_dimension + j] = 0;
-			for (unsigned int k = 0; k < (sq_dimension>>4)*16; k+=16) {
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * 
-														sq_matrix_2_tmp[j*sq_dimension + k];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+1] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+1];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+2] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+2];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+3] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+3];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+4] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+4];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+5] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+5];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+6] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+6];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+7] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+7];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+8] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+8];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+9] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+9];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+10] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+10];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+11] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+11];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+12] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+12];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+13] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+13];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+14] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+14];
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k+15] * 
-														sq_matrix_2_tmp[j*sq_dimension + k+15];
-
-			}
-			for (unsigned int k = (sq_dimension>>4)*16; k < sq_dimension; k++) {
-				sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * 
-														sq_matrix_2_tmp[j*sq_dimension + k];
+	
+	if(sq_dimension%4 == 0)
+		#pragma omp parallel for if(sq_dimension > 200)
+		for (unsigned int i = 0; i < sq_dimension; i++) 
+		{
+			for(unsigned int j = 0; j < sq_dimension; j++) 
+			{       
+				__m128 dest = (__m128)_mm_setzero_si128();
+				sq_matrix_result[i*sq_dimension + j] = 0;
+				for (unsigned int k = 0; k < (sq_dimension>>2)*4; k+=4) {
+					__m128* src1 = (__m128*)&sq_matrix_1[i*sq_dimension + k];
+					__m128* src2 = (__m128*)&sq_matrix_2_tmp[j*sq_dimension + k];
+					dest = _mm_add_ps(dest, _mm_mul_ps(*src1, *src2));
+				}
+				float * dest_f = (float *)&dest;
+				for(int p=0; p<4; p++) {
+					sq_matrix_result[i*sq_dimension + j] += (*dest_f++);
+				}
+				for (unsigned int k = (sq_dimension>>2)*4; k < sq_dimension; k++) {
+					sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * 
+															sq_matrix_2_tmp[j*sq_dimension + k];
+				}
 			}
 		}
-	}
+	else
+		#pragma omp parallel for if(sq_dimension > 200)
+		for (unsigned int i = 0; i < sq_dimension; i++) 
+		{
+			for(unsigned int j = 0; j < sq_dimension; j++) 
+			{       
+				sq_matrix_result[i*sq_dimension + j] = 0;
+				for (unsigned int k = 0; k < sq_dimension; k++)
+					sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * 
+															sq_matrix_2_tmp[j*sq_dimension + k];
+			}
+		}
 }
   
 } //namespace omp
+
+/* version 3
+#include <omp.h>
+#include <iostream>
+#include <emmintrin.h>
+#include "matrix_mul.h"
+
+namespace omp
+{
+
+void matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, 
+	                       float *sq_matrix_result, unsigned int sq_dimension )
+{
+	float *sq_matrix_2_tmp = new float[sq_dimension * sq_dimension];
+#pragma omp parallel for 
+    for(unsigned int i = 0; i < sq_dimension; i++) {
+		for(unsigned int j = 0; j < sq_dimension; j++){
+			sq_matrix_2_tmp[i*sq_dimension+j] = sq_matrix_2[j*sq_dimension+i];
+		}
+	}
+	
+	if(sq_dimension%4 == 0)
+		#pragma omp parallel for if(sq_dimension > 200)
+		for (unsigned int i = 0; i < sq_dimension; i++) 
+		{
+			for(unsigned int j = 0; j < sq_dimension; j++) 
+			{       
+				__m128 dest = (__m128)_mm_setzero_si128();
+				sq_matrix_result[i*sq_dimension + j] = 0;
+				for (unsigned int k = 0; k < (sq_dimension>>2)*4; k+=4) {
+					__m128* src1 = (__m128*)&sq_matrix_1[i*sq_dimension + k];
+					__m128* src2 = (__m128*)&sq_matrix_2_tmp[j*sq_dimension + k];
+					dest = _mm_add_ps(dest, _mm_mul_ps(*src1, *src2));
+				}
+				float * dest_f = (float *)&dest;
+				for(int p=0; p<4; p++) {
+					sq_matrix_result[i*sq_dimension + j] += (*dest_f++);
+				}
+				for (unsigned int k = (sq_dimension>>2)*4; k < sq_dimension; k++) {
+					sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * 
+															sq_matrix_2_tmp[j*sq_dimension + k];
+				}
+			}
+		}
+	else
+		#pragma omp parallel for if(sq_dimension > 200)
+		for (unsigned int i = 0; i < sq_dimension; i++) 
+		{
+			for(unsigned int j = 0; j < sq_dimension; j++) 
+			{       
+				sq_matrix_result[i*sq_dimension + j] = 0;
+				for (unsigned int k = 0; k < sq_dimension; k++)
+					sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * 
+															sq_matrix_2_tmp[j*sq_dimension + k];
+			}
+		}
+}
+  
+} //namespace omp
+*/
 
 /* version 2
 #include <omp.h>
