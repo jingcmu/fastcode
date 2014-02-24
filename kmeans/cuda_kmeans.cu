7b8ec295584e4ee3779bd03d4d6f7e7574e21751
Yifan Cao <yifanc@cmu.edu>
2014-02-24 18:17:38 -0500

    change from unsigned char to unsigned short

--- kmeans/cuda_kmeans.cu
+++ kmeans/cuda_kmeans.cu
@@ -90,12 +90,12 @@ void find_nearest_cluster(int numCoords,
                           int *membership,          //  [numObjs]
                           int *intermediates)
 {
-    extern __shared__ char sharedMemory[];
+    extern __shared__ short sharedMemory[];
 
     //  The type chosen for membershipChanged must be large enough to support
     //  reductions! There are blockDim.x elements, one for each thread in the
     //  block.
-    unsigned char *membershipChanged = (unsigned char *)sharedMemory;
+    unsigned short *membershipChanged = (unsigned short *)sharedMemory;
     float *clusters = (float *)(sharedMemory + blockDim.x);
 
     membershipChanged[threadIdx.x] = 0;
@@ -251,11 +251,11 @@ float** cuda_kmeans(float **objects,      /* in: [numObjs][numCoords] */
     //  two, and it *must* be no larger than the number of bits that will
     //  fit into an unsigned char, the type used to keep track of membership
     //  changes in the kernel.
-    const unsigned int numThreadsPerClusterBlock = 128;
+    const unsigned int numThreadsPerClusterBlock = 512;
     const unsigned int numClusterBlocks =
         (numObjs + numThreadsPerClusterBlock - 1) / numThreadsPerClusterBlock;
     const unsigned int clusterBlockSharedDataSize =
-        numThreadsPerClusterBlock * sizeof(unsigned char) +
+        numThreadsPerClusterBlock * sizeof(unsigned short) +
         numClusters * numCoords * sizeof(float);
 
     const unsigned int numReductionThreads =
